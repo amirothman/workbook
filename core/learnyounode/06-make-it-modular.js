a368647291a5e8a027d0220a16300c76fa0ce9d7
Amir Othman <othman.amir@gmail.com>
2014-09-15 21:30:51 +0200

    one more time mofos

--- core/learnyounode/06-make-it-modular.js
+++ core/learnyounode/06-make-it-modular.js
@@ -1,1 +1,14 @@
-var mymodule = require('./mymodule.js')
+var mymodule = require('./06module.js')
+var fs = require('fs');
+var path = process.argv[2];
+
+var extension = process.argv[3]
+
+mymodule(path,extension,function (err, files) {
+	if (err) {
+		process.exit(1);
+	}
+	files.forEach(function (file) {
+		console.log(file);
+	});
+});
--- core/learnyounode/06module.js
+++ core/learnyounode/06module.js
@@ -1,3 +1,26 @@
-module.exports = function (){
-	
+var fs = require('fs');
+//var path = process.argv[2];
+
+//var extension2 = process.argv[3]
+//var extension = '\.'+process.argv[3]+'$';
+//var pattern = new RegExp(extension);
+
+module.exports = function (path,extension2,callback){
+
+	fs.readdir(path, function (err,files){
+		//files is an array of filenames
+		if (err) return callback(err);
+
+		extension = '\.'+extension2+'$';
+		pattern = new RegExp(extension);
+
+		filtered = []
+		files.forEach(function(v,i){
+			if (v.match(pattern))
+				filtered.push(v);
+		});
+	  	
+	  callback(null,filtered)
+	});
+
 }
